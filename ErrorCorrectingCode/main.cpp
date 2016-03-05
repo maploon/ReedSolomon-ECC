29a22261fc17a8c1409afb92b1e3407fec76467e
Feng Gui <feng.gui@duke.edu>
2016-03-05 16:47:32 -0500

    Initial Commit

---
+++ ErrorCorrectingCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ErrorCorrectingCode
+//
+//  Created by Feng Gui on 3/5/16.
+//  Copyright © 2016 Feng Gui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
